d356596d290b39f0c4f2c78af7fb41e2a38a5913
Jaypes35 <63829040+Jaypes35@users.noreply.github.com>
2020-06-30 15:40:42 -0400

    Create rotation.js

---
+++ rotation.js
@@ -0,0 +1,10 @@
+
+ handleRotation(event) {
+    if (isMarkerVisible) {
+      el.object3D.rotation.y +=
+        event.detail.positionChange.x * rotationFactor;
+
+      el.object3D.rotation.x +=
+        event.detail.positionChange.y * rotationFactor;
+    }
+  }
